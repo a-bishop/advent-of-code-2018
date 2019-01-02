a0faed7e3cb284549b855970bdd68816e6b8a3af
Andrew Bishop <andrew.bishop53@gmail.com>
2019-01-01 17:50:50 -0800

    solved day 5 part 2

--- day5/day5-solution.py
+++ day5/day5-solution.py
@@ -1,5 +1,8 @@
+from string import ascii_lowercase
+from collections import Counter
 
-def solve1():
+def solve1(polymers):
+  newPolymers = []
   for letter in polymers:
     if letter.isupper():
       if len(newPolymers) == 0:
@@ -17,7 +20,17 @@ def solve1():
         newPolymers.pop()
   return len(newPolymers)
 
+def solve2(polymers):
+  lengthCount = Counter()
+  for letter in ascii_lowercase:
+    bufString = ''
+    for polymer in polymers:
+      if polymer.lower() != letter:
+        bufString += polymer
+    lengthCount[letter] = solve1(bufString)
+  return lengthCount.most_common()[-1]
+
 with open ('day5-input.txt', 'r') as f:
-  polymers = f.read().strip()
-  newPolymers = []
-  print(solve1())
+  mainPolymers = f.read().strip()
+  print(solve1(mainPolymers))
+  print(solve2(mainPolymers))
